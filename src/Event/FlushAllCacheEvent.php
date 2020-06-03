c021961e88919f7e7b72df0cbf782a763f036301
Paul Water <p.r.water@setbased.nl>
2020-06-03 06:07:57 +0200

    Initial commit.

---
+++ src/Event/FlushAllCacheEvent.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Plaisio\Cache\Event;
+
+/**
+ * Event fired when all caches must be flushed.
+ */
+class FlushAllCacheEvent
+{
+  // Nothing to implement.
+}
+
+//----------------------------------------------------------------------------------------------------------------------
